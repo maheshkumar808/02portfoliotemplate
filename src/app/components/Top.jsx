d3d87a9499549f93860a59408909166cdc674596
maheshkumar808 <808maheshkumar@gmail.com>
2025-03-03 17:56:01 +0530

    heading change

--- src/app/components/Top.jsx
+++ src/app/components/Top.jsx
@@ -13,7 +13,7 @@ function Top() {
           </div>
           <div className="w-full md:col-span-8 p-8 text-left">
             <h1 className="text-5xl text-black">
-              Hi. I'm <strong>Harish</strong>
+              Hi. I'm <strong>John Doe</strong>
             </h1>
             <p className="text-2xl text-gray-600 leading-loose py-8">
               And this is{' '}
